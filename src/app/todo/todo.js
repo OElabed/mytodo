47a7ae6c29c2ab7de0ae1daac7585c627d83e597
oelabed <oel3abed@gmail.com>
2016-09-08 23:18:09 +0200

    [DEV] Managing toggle todo

--- src/app/todo/todo.js
+++ src/app/todo/todo.js
@@ -19,7 +19,7 @@
         }])
 
 
-        .controller('TodoCtrl', ['$scope', function ($scope) {
+        .controller('TodoCtrl', ['$scope', '$filter', function ($scope, $filter) {
 
 
             $scope.todos = [
@@ -28,23 +28,59 @@
                     completed: false
                 }, {
                     name: 'tache 2',
-                    completed: true
+                    completed: false
                 }
             ];
 
+
+
             $scope.remaining = $scope.todos.length;
+
+            $scope.toggleAll = false;
 
             $scope.delete = function (index) {
                 $scope.todos.splice(index, 1);
                 $scope.remaining = $scope.remaining - 1;
             };
 
-            $scope.addTodo = function(){
-               if ($scope.newTodo) {
-                   $scope.todos.push({name : $scope.newTodo, completed : false});
-                   $scope.newTodo = '';
-               } 
+            $scope.addTodo = function () {
+                if ($scope.newTodo) {
+                    $scope.todos.push({ name: $scope.newTodo, completed: false });
+                    $scope.newTodo = '';
+                }
             };
+
+
+            $scope.$watch('todos', function () {
+                $scope.remaining = $filter('filter')($scope.todos, { completed: false }).length;
+                if ($scope.remaining) {
+                    $scope.toggleAll = false;
+                } else {
+                    $scope.toggleAll = true;
+                }
+            }, true);
+
+
+            $scope.toggleAllCompleted = function (toggleAll) {
+                if (toggleAll) {
+                    angular.forEach($scope.todos, function (value) {
+                        if (!value.completed) {
+                            value.completed = true;
+                        }
+                    });
+                } else {
+                    angular.forEach($scope.todos, function (value) {
+                        if (value.completed) {
+                            value.completed = false;
+                        }
+                    });
+                }
+            };
+
+            $scope.editTodo = function(todoEditing) {
+                
+            };
+
 
         }]);
 
