370b8607cdb152ea0191edbc0710a6c7a1d88ea8
oelabed <oel3abed@gmail.com>
2016-09-08 18:05:43 +0200

    [DEV] add push Todos

--- src/app/todo/todo.js
+++ src/app/todo/todo.js
@@ -21,6 +21,31 @@
 
         .controller('TodoCtrl', ['$scope', function ($scope) {
 
+
+            $scope.todos = [
+                {
+                    name: 'tache 1',
+                    completed: false
+                }, {
+                    name: 'tache 2',
+                    completed: true
+                }
+            ];
+
+            $scope.remaining = $scope.todos.length;
+
+            $scope.delete = function (index) {
+                $scope.todos.splice(index, 1);
+                $scope.remaining = $scope.remaining - 1;
+            };
+
+            $scope.addTodo = function(){
+               if ($scope.newTodo) {
+                   $scope.todos.push({name : $scope.newTodo, completed : false});
+                   $scope.newTodo = '';
+               } 
+            };
+
         }]);
 
 })();
